4432f2ac5380a8514f4234a3b418eb9873a20f77
Nazarshia2889 <arshia.nayebnazar@yahoo.com>
2023-07-02 10:34:21 -0700

    initial
    exit

---
+++ src/test/java/com/example/EarthSearch/EarthSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EarthSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarthSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
